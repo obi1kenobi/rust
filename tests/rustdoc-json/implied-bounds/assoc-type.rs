c9fa13bc9f81b5139439793e696802e894fd6398
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-11-27 12:27:55 -0500

    Merge test files.

--- tests/rustdoc-json/implied-bounds/assoc-type.rs
+++ tests/rustdoc-json/implied-bounds/assoc-type.rs
@@ -4,11 +4,15 @@ impl<T: Sized> SizedOnly for T {}
 pub trait StaticOnly: 'static {}
 impl<T: 'static> StaticOnly for T {}
 
-//@ has "$.index[?(@.name=='Item')].inner.assoc_type.bounds[?(@.trait_bound.trait.path=='Sized' && @.trait_bound.modifier=='maybe')]"
-//@ has "$.index[?(@.name=='Item')].inner.assoc_type.implied_bounds[?(@.trait_bound.trait.path=='Sized' && @.trait_bound.modifier=='none')]"
-//@ !has "$.index[?(@.name=='Item')].inner.assoc_type.implied_bounds[?(@.trait_bound.trait.path=='SizedOnly')]"
 pub trait Container {
+    //@ has "$.index[?(@.name=='Item')].inner.assoc_type.bounds[?(@.trait_bound.trait.path=='Sized' && @.trait_bound.modifier=='maybe')]"
+    //@ has "$.index[?(@.name=='Item')].inner.assoc_type.implied_bounds[?(@.trait_bound.trait.path=='Sized' && @.trait_bound.modifier=='none')]"
+    //@ !has "$.index[?(@.name=='Item')].inner.assoc_type.implied_bounds[?(@.trait_bound.trait.path=='SizedOnly')]"
     type Item: SizedOnly + ?Sized;
+
+    //@ has "$.index[?(@.name=='UnsizedItem')].inner.assoc_type.bounds[?(@.trait_bound.modifier=='maybe' && @.trait_bound.trait.path=='Sized')]"
+    //@ !has "$.index[?(@.name=='UnsizedItem')].inner.assoc_type.implied_bounds[?(@.trait_bound.trait.path=='Sized')]"
+    type UnsizedItem: ?Sized;
 }
 
 //@ has "$.index[?(@.name=='Output')].inner.assoc_type.implied_bounds[?(@.trait_bound.trait.path=='Sized' && @.trait_bound.modifier=='none')]"
